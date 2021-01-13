73ff4a1bb23b1d03622705366d300019d4f66c45
Hakeem Davis <hakeem.davis@identityfusion.com>
2021-01-13 13:30:55 -0500

    Add first task

---
+++ taskOne.py
@@ -0,0 +1,22 @@
+users = [
+    { 'name': 'steve', 'title': 'developer', 'salary': 50000 },
+    { 'name': 'chris', 'title': 'project manager', 'salary': 65000 },
+    { 'name': 'tom', 'title': 'developer', 'salary': 75000 },
+    { 'name': 'jerry', 'title': 'consultant', 'salary': 85000 },
+    { 'name': 'bryc', 'title': 'developer', 'salary': 60000 }
+]
+
+# We need to give all of our developers a $5,000 raise.
+
+# Please iterate through the list of users and add $5,000 to the salary for any employee with the title of "developer" and print each user to the screen as you iterate through the list.
+
+# Desired result - The user list should be updated so that each developer's salary is increased by $5,000.
+
+# Example result:
+# User salary updated: {'name': 'steve', 'title': 'developer', 'salary': 55000}
+# User salary not updated: {'name': 'chris', 'title': 'project manager', 'salary': 65000}
+# User salary updated: {'name': 'tom', 'title': 'developer', 'salary': 80000}
+# User salary not updated: {'name': 'jerry', 'title': 'consultant', 'salary': 85000}
+# User salary updated: {'name': 'bryc', 'title': 'developer', 'salary': 65000}
+
+
